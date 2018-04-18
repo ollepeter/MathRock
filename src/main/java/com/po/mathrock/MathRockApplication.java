31f609bae7557a33d7b4e8da54232832952f147c
ollepeter <mr.peterolle@gmail.com>
2018-04-18 18:20:51 +0200

    Initial commit

---
+++ src/main/java/com/po/mathrock/MathRockApplication.java
@@ -0,0 +1,12 @@
+package com.po.mathrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathRockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathRockApplication.class, args);
+    }
+}
---
+++ src/test/java/com/po/mathrock/MathRockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.po.mathrock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MathRockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
