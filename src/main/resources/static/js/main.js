70c6a17efb9701d4ba4abb3d5205ff2816372d36
ollepeter <mr.peterolle@gmail.com>
2018-04-21 12:55:35 +0200

    Test eventListener in js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    alert("Hello! How are you?");
-}
-
-document.getElementById("testjs").addEventListener("click",myFunction);
-
-$("#testjquery").click(myFunction);
+// function myFunction() {
+//     alert("Hello! How are you?");
+// }
+//
+// document.getElementById("testjs").addEventListener("click",myFunction);
+//
+// $("#testjquery").click(myFunction);
 
