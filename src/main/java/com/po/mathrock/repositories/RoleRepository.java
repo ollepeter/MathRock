9e185702059ba77511c39e20226e26ef1a478fc0
ollepeter <mr.peterolle@gmail.com>
2018-05-07 09:42:51 +0200

    change from CrudRepository to JpaRepository interface

--- src/main/java/com/po/mathrock/repositories/RoleRepository.java
+++ src/main/java/com/po/mathrock/repositories/RoleRepository.java
@@ -4,6 +4,6 @@ import com.po.mathrock.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RoleRepository extends CrudRepository<Role,Integer> {
+public interface RoleRepository extends JpaRepository<Role,Integer> {
     Role findByRoleName(String role);
 }
--- src/main/java/com/po/mathrock/repositories/UserRepository.java
+++ src/main/java/com/po/mathrock/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import com.po.mathrock.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername(String username);
 
